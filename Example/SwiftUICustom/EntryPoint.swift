1921c9653bff93d5ef5839b2a3d9b33d2e00b8c3
jrosen081 <jrosen081@gmail.com>
2021-06-21 21:32:33 -0700

    Added App type and one scene config

---
+++ Example/SwiftUICustom/EntryPoint.swift
@@ -0,0 +1,19 @@
+//
+//  EntryPoint.swift
+//  SwiftUICustom_Example
+//
+//  Created by Jack Rosen on 6/21/21.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import SwiftUICustom
+
+@main
+struct MyApp: App {
+    var scene: some Scene {
+        WindowGroup {
+            SampleView()
+        }
+    }
+}
